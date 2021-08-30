dba509b7367a505dc788a4bd61ba872aab86f13b
yrs-rosh <roshankr2110@gmail.com>
2021-08-30 12:30:15 +0530

    30-08-2021 submission

---
+++ PrimeTillN.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Main {
+  public static void main(String[] args) {
+// write your code here
+    Scanner scn = new Scanner(System.in);
+    int low = scn.nextInt();
+    int high = scn.nextInt();
+    boolean check = true;
+    for (int i = low;i<=high ; i++){
+        for (int j = 2 ; j * j <= i ; j++) {
+      if (i % j != 0) {
+          check = true;
+        } else {
+          check = false;
+          break;
+        }
+
+      }
+      if (check) {
+    System.out.println(i);
+    }
+    }
+    
+  }
+}
---
+++ printZ.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Write your code here
+        System.out.print("*****\n   *\n  *\n *\n*****");
+
+    }
+}
