2613fe0d3c2b6faac74d36b50fb4886fad7a81d4
yrs-rosh <roshankr2110@gmail.com>
2021-09-06 11:23:57 +0530

    06-09-2021 noon

---
+++ Patterns/Pattern12.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        // write ur code here
+        int n = scn.nextInt();
+        int a=0;
+        int b=1;
+        for(int i=1 ; i<=n ; i++){
+            for(int j=1 ; j<=i ; j++){
+                System.out.print(a+"\t");
+                int f=a+b;
+                a=b;
+                b=f;
+            }
+            System.out.println();
+        }
+
+    }
+}
