36ece2879f223920e773d035e0d81be39a07800e
rosh <roshankr2110@gmail.com>
2021-09-13 15:21:20 +0530

    13 sept afternoon

--- Getting Started/10.RotateANumber.java
+++ Getting Started/10.RotateANumber.java
@@ -3,34 +3,46 @@ import java.util.*;
    public class Main{
    
    public static void main(String[] args) {
-     // write your code here 
+     // write your code here
      Scanner scn = new Scanner(System.in);
      int n = scn.nextInt();
      int k = scn.nextInt();
      int temp = n;
-     int count =0;
-     int front = 1;
-     int back = 1;
-     for(int i=0 ; i<=10 ; i++){
-         temp=temp/10;
-         count++;
-         if(temp==0){
+     int num = n;
+     int back = 0;
+     int front = 0;
+     int div = 10;
+     
+     int count = 0;
+     
+     for(int i=1 ; i<=10 ; i++){
+         if(temp<=0){
              break;
          }
-     }
-     if(k>0){
-         front = n/(int)Math.pow(10,k);
-         back = n % (int)Math.pow(10,k);
-         System.out.print(back * (int)Math.pow(10,(count-k)) + front);
-         
-     }else{
-         k=k*-1;
-         front = n/(int)Math.pow(10,count-k);
-         back = n % (int)Math.pow(10,count-k);
-         System.out.print(back * (int)Math.pow(10,k) + front);
+         temp = temp/10;
+         count++;
      }
      
-
-
+     if(k>=0){
+         k=k % count;
+         for(int j=1 ; j<=k ;j++){
+         front = num/10;
+         back = num%div;
+         num = back*(int)Math.pow(10,count-1) + front;
+        }
+     }else{
+         k = k * -1;
+         k=k%count;
+         for(int j=1 ; j<=k ;j++){
+         front = num/(int)Math.pow(10,count-1);
+         back = num%(int)Math.pow(10,count-1);
+         num = back*10 + front;
+        }
+         
+         
+     }
+     
+     
+     System.out.print(num);
     }
    }
