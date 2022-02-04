5786e6f8ee87388772dd697ff13b1e1d22a380cc
rosh <roshankr2110@gmail.com>
2022-02-04 23:56:51 +0530

    4 feb midnight

---
+++ Recursion & Backtracking/Recursion in arrays/DisplayArrayinReverse.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+  public static void main(String[] args) throws Exception {
+    // write your code here
+    Scanner scn = new Scanner(System.in);
+    int n = scn.nextInt();
+    int[] arr = new int[n];
+    for (int i = 0 ; i < arr.length ; i++) {
+      arr[i] = scn.nextInt();
+    }
+    displayArr(arr, n - 1);
+  }
+
+  public static void displayArr(int[] arr, int idx) {
+    if (idx < 0) {
+      return;
+    }
+    System.out.println(arr[idx]);
+    displayArr(arr, idx - 1);
+  }
+
+}
